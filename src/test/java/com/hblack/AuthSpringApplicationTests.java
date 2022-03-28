7036342eb153a8ad5c7398ad0e76e37ca327a833
vkolosovvv <madrat444>
2022-03-28 17:36:02 +0300

    init

---
+++ src/test/java/com/hblack/AuthSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hblack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
