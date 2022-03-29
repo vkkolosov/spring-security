7e3d3e5500c191012d82c6e9e0fda3beb2d4b1fc
Vitaliy Kolosov <vk.kolosov@gmail.com>
2022-03-29 14:31:17 +0300

    @requestbody

--- src/main/java/com/hblack/controller/AuthController.java
+++ src/main/java/com/hblack/controller/AuthController.java
@@ -5,6 +5,7 @@ import org.json.simple.JSONObject;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,7 +17,7 @@ public class AuthController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity login(JSONObject Body) {
+    public ResponseEntity login(@RequestBody JSONObject Body) {
         return ResponseEntity.ok("Request done");
     }
 
